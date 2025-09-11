a1fb394d2498183f96fe0ab3231f3b72e7b47b78
IMMINI3626 <gkswjdals0306@gmail.com>
2025-09-11 19:27:28 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int A;
+	int B;
+
+	scanf("%d %d", &A, &B);
+
+
+	int C = 0;
+	C = A * B;
+
+	printf("%d\n", C);
+
+	return 0;
+}
