da34ccf0e7433a9869014462a0d2b8b93fa6dab2
IMMINI3626 <gkswjdals0306@gmail.com>
2026-01-02 11:25:25 +0900

    [Bronze V] Title: A+B - 4, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(a +b)
+    except:
+        break
+
+# try - except
+# try :
+#     실행할 코드
+# except :
+#     예외가 발생할 때 실행할 코드
